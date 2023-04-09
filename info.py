d11395191582fe5a6ba5dc7337c7607f4f352977
Ayushkumar Patel <106531575+ayush1104@users.noreply.github.com>
2023-04-09 17:50:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ayush1104:X7hLBY6naB05llpt@ayush.qetbcuz.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ayush1104:X7hLBY6naB05IIpt@ayush.qetbcuz.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ayush")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ayush')
 
